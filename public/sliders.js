454d5252d4ddbad249c2de7e4eda2f5acae2d1c0
James <jlauritsen04@gmail.com>
2022-12-09 14:39:55 -0500

    camelcase implementation

--- public/sliders.js
+++ public/sliders.js
@@ -1,14 +1,14 @@
 function navCheck() {
-    document.querySelector('.checkbox').checked = false;
+    document.querySelector('.checkBox').checked = false;
 }
 
 function menuVanish() {
     var x = document.getElementById("topNav");
-    if (x.className === "menu-items") {
+    if (x.className === "menuItems") {
         x.className += " jsVanish";
-      x.className -= "menu-items";
+      x.className -= "menuItems";
     } else {
-      x.className += " menu-items";
+      x.className += " menuItems";
       x.className -= "jsVanish";
     }
   }
@@ -74,25 +74,25 @@ function slide1In() {
 
     
     //Paragraph
-    const slide1W7 = document.querySelectorAll('.paragraph-layer');
+    const slide1W7 = document.querySelectorAll('.paragraphLayer');
     for (let i = 0; i < slide1W7.length; i++) {   
         slide1W7[i].style.animationName = 'slide1-wrapperP2';
     }
 
     //First Name
-    const slide1W = document.querySelectorAll('.first-name-wrapper');
+    const slide1W = document.querySelectorAll('.firstNameWrapper');
     for (let i = 0; i < slide1W.length; i++) {   
         slide1W[i].style.animationName = 'slide1-wrapperFN2';
     }
 
     //Last Name
-    const slide1W2 = document.querySelectorAll('.last-name-wrapper');
+    const slide1W2 = document.querySelectorAll('.lastNameWrapper');
     for (let i = 0; i < slide1W2.length; i++) {   
         slide1W2[i].style.animationName = 'slide1-wrapperLN2';
     }
 
     //Rocket
-    const slide1W3 = document.querySelectorAll('.rocket-wrapper');
+    const slide1W3 = document.querySelectorAll('.rocketWrapper');
     for (let i = 0; i < slide1W3.length; i++) {   
         slide1W3[i].style.animationName = 'rocketIn';
     }
@@ -109,25 +109,25 @@ function slide1Out() {
     element.classList.remove("selected");
 
     //First Name
-    const slide1W = document.querySelectorAll('.first-name-wrapper');
+    const slide1W = document.querySelectorAll('.firstNameWrapper');
     for (let i = 0; i < slide1W.length; i++) {   
         slide1W[i].style.animationName = 'slide1-wrapperFN';
     }
 
     //Last Name
-    const slide1W2 = document.querySelectorAll('.last-name-wrapper');
+    const slide1W2 = document.querySelectorAll('.lastNameWrapper');
     for (let i = 0; i < slide1W2.length; i++) {   
         slide1W2[i].style.animationName = 'slide1-wrapperLN';
     }
 
     //Rocket
-    const slide1W3 = document.querySelectorAll('.rocket-wrapper');
+    const slide1W3 = document.querySelectorAll('.rocketWrapper');
     for (let i = 0; i < slide1W3.length; i++) {   
         slide1W3[i].style.animationName = 'rocketOut';
     }
 
     //Paragraph
-    const slide1W7 = document.querySelectorAll('.paragraph-layer');
+    const slide1W7 = document.querySelectorAll('.paragraphLayer');
     for (let i = 0; i < slide1W7.length; i++) {   
         slide1W7[i].style.animationName = 'slide1-wrapperP';
     }
@@ -152,7 +152,7 @@ function slide2In() {
     //Make button unclickable
     document.getElementById('slide2').disabled = true;
 
-    const slide1W9a = document.querySelectorAll('.SkillsWrapper');
+    const slide1W9a = document.querySelectorAll('.skillsWrapper');
         for (let i = 0; i < slide1W9a.length; i++) {   
         slide1W9a[i].style.display = 'flex';
         }
@@ -160,21 +160,21 @@ function slide2In() {
     //Adds display back and checks window size. If mobile, changes to make skills list go below paragraph.
     function skillsIn(min200) {
         if (min200.matches) {
-            const slide1W9b = document.querySelectorAll('.BallWrapper');
+            const slide1W9b = document.querySelectorAll('.ballWrapper');
             for (let i = 0; i < slide1W9b.length; i++) {   
             slide1W9b[i].style.float = 'left'; 
             }
-            const slide1W9h = document.querySelectorAll('.SkillsWrapper');
+            const slide1W9h = document.querySelectorAll('.skillsWrapper');
             for (let i = 0; i < slide1W9h.length; i++) {   
                 slide1W9h[i].style.display = 'block'; 
             }
         }
         else {
-            const slide1W9g = document.querySelectorAll('.BallWrapper');
+            const slide1W9g = document.querySelectorAll('.ballWrapper');
             for (let i = 0; i < slide1W9g.length; i++) {   
                 slide1W9g[i].style.float = 'right'; 
             }
-            const slide1W9a = document.querySelectorAll('.SkillsWrapper');
+            const slide1W9a = document.querySelectorAll('.skillsWrapper');
             for (let i = 0; i < slide1W9a.length; i++) {   
             slide1W9a[i].style.display = 'flex'; 
             }
@@ -184,11 +184,11 @@ function slide2In() {
     skillsIn(min200)
 
     //Change animation name to add chart
-    const slide1W = document.querySelectorAll('.SkillsWrapper');
+    const slide1W = document.querySelectorAll('.skillsWrapper');
     for (let i = 0; i < slide1W.length; i++) {   
         slide1W[i].style.animationName = 'windowIn';
     }
-    const slide1W31 = document.querySelectorAll('.SkillsWrapper');
+    const slide1W31 = document.querySelectorAll('.skillsWrapper');
         for (let i = 0; i < slide1W31.length; i++) {   
             slide1W31[i].style.animationDuration = '1s';
             }
@@ -207,11 +207,11 @@ function slide2Out() {
         element2.classList.remove("selected");
 
         //Change animation name to remove chart
-        const slide1W11 = document.querySelectorAll('.SkillsWrapper');
+        const slide1W11 = document.querySelectorAll('.skillsWrapper');
         for (let i = 0; i < slide1W11.length; i++) {   
         slide1W11[i].style.animationName = 'windowOut';
             }
-            const slide1W31 = document.querySelectorAll('.SkillsWrapper');
+            const slide1W31 = document.querySelectorAll('.skillsWrapper');
         for (let i = 0; i < slide1W31.length; i++) {   
             slide1W31[i].style.animationDuration = '5s';
             }
@@ -219,7 +219,7 @@ function slide2Out() {
     //Remove display with 1 second delay for animations.
     setTimeout(disappear2, 1000)
     function disappear2() {
-        const slide1W9a = document.querySelectorAll('.SkillsWrapper');
+        const slide1W9a = document.querySelectorAll('.skillsWrapper');
         for (let i = 0; i < slide1W9a.length; i++) {   
         slide1W9a[i].style.display = 'none';
         }
@@ -231,18 +231,18 @@ function slide3In() {
     //Make button unclickable
     document.getElementById('slide3').disabled = true;
 
-    const slide1W9ab = document.querySelectorAll('.projects-page');
+    const slide1W9ab = document.querySelectorAll('.projectsPage');
     for (let i = 0; i < slide1W9ab.length; i++) {   
         slide1W9ab[i].style.display = 'flex';
     }
 
     //Adding display back.
-    const slide1W9a = document.querySelectorAll('.projects-page');
+    const slide1W9a = document.querySelectorAll('.projectsPage');
         for (let i = 0; i < slide1W9a.length; i++) {   
         slide1W9a[i].style.display = 'block';
         }
     //Change animation name to input.
-    const slide1W = document.querySelectorAll('.projects-wrapper');
+    const slide1W = document.querySelectorAll('.projectsWrapper');
     for (let i = 0; i < slide1W.length; i++) {   
         slide1W[i].style.animationName = 'wrapperIn';
     }
@@ -260,7 +260,7 @@ function slide3Out() {
         element2.classList.remove("selected");
 
         //Change animation name to remove chart
-        const slide1W11 = document.querySelectorAll('.projects-wrapper');
+        const slide1W11 = document.querySelectorAll('.projectsWrapper');
         for (let i = 0; i < slide1W11.length; i++) {   
         slide1W11[i].style.animationName = 'wrapperOut';
             }
@@ -268,7 +268,7 @@ function slide3Out() {
     //Remove display with 1 second delay for animations.
     setTimeout(disappear3, 1000)
     function disappear3() {
-        const slide1W9a = document.querySelectorAll('.projects-page');
+        const slide1W9a = document.querySelectorAll('.projectsPage');
         for (let i = 0; i < slide1W9a.length; i++) {   
         slide1W9a[i].style.display = 'none';
         }
